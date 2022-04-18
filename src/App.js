4bdad7c1508aedc3bc529b5107570e52ef039b80
Joaquín Mato <jmato.dev@gmail.com>
2022-04-17 22:00:51 -0300

    feat: integrates Tasks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Header } from './components';
+import React, { useState } from 'react';
+import { Header, Tasks } from './components';
 
 const App = function App() {
+  const [tasks, setTasks] = useState([
+    {
+      text: 'Doctors Appointment',
+      id: 1,
+      date: 'Feb 5th at 5:30pm',
+      reminder: true,
+    },
+    {
+      text: 'Meeting at School',
+      id: 2,
+      date: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      text: 'Food Shopping',
+      id: 3,
+      date: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="App">
       <Header />
-      React Task Tracker
+      <Tasks tasks={tasks} />
     </div>
   );
 };
