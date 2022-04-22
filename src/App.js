a4607af9ae70ebd8cc200a2cf357b81ad030f8cd
Joaquín Mato <jmato.dev@gmail.com>
2022-04-21 22:03:21 -0300

    fix: remove tasks array

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ import { Header, Tasks, TaskAdd } from './components';
 import './App.css';
 
 const App = function App() {
-  const [tasks, setTasks] = useState([
-    {
-      text: 'Doctors Appointment',
-      id: 1,
-      date: 'Feb 5th at 5:30pm',
-      reminder: true,
-    },
-    {
-      text: 'Meeting at School',
-      id: 2,
-      date: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      text: 'Food Shopping',
-      id: 3,
-      date: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [showAddForm, setShowAddForm] = useState(false);
 
   const addTask = (text, date, reminder) => {
