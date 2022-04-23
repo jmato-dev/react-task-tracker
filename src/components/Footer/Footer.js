5a0f07dec2057afd857b96420ca066f5334c3d56
Joaquín Mato <jmato.dev@gmail.com>
2022-04-23 09:08:16 -0300

    feat: create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = function Footer() {
+  return (
+    <footer>
+      Copyright &copy; 2022
+      <a href="/about">About</a>
+    </footer>
+  );
+};
+
+export { Footer as default };
