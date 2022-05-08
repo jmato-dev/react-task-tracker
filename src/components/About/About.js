5807dce57bf9cc4f9a5546a102cca83acce08a29
Joaquín Mato <jmato.dev@gmail.com>
2022-05-08 03:16:12 -0300

    feat: get version from server

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Throbber from '../Throbber';
 
 const About = function About() {
+  // const dependency = true;
+
+  const [fetching, setFetching] = useState(false);
+  const [version, setVersion] = useState('');
+
+  const getJSON = async (url) => {
+    setFetching(true);
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setFetching(false);
+
+    return data;
+  };
+
+  useEffect(() => {
+    const getVersion = async () => {
+      const data = await getJSON('http://192.168.0.231:5000/system');
+
+      if (version !== data.version) {
+        setVersion(data.version);
+      }
+    };
+
+    getVersion();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <section className="about" aria-label="main">
-      <h4>version 0.1.0</h4>
+      <h4>
+        <span>version</span>
+        {!fetching ? <span className="version">{version}</span> : <Throbber />}
+      </h4>
       <Link to="/">Go Back</Link>
     </section>
   );
